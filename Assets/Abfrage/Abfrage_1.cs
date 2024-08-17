f20a07bc5117928e1fb2efa614660ca2e029fc31
Max <jugj@users.noreply.github.com>
2024-08-17 15:40:57 +0200

    Änderungen von Max um 15:40:57

--- Assets/Abfrage/Abfrage_1.cs
+++ Assets/Abfrage/Abfrage_1.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.PlayerLoop;
 
 public class Abfrage : MonoBehaviour
 {
+    public GameObject Player;
+    string Raum = "Raum_1";
+    int Level = 1;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,40 @@ public class Abfrage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if()
+        GameObject[] Monster = GameObject.FindGameObjectsWithTag("Monster");
+
+        if(Monster.Length ==0){
+
+            Player.transform.position = new Vector2()
+
+        
+{}      
+        switch(Raum){
+
+        case "Raum_1":
+
+        switch(Level)
+
+        case 1:
+
+        Instantiate(GameObject.Find("Slime"),new Vector2(-16, 5));
+
+
+        break;
+
+        case constant-expression2 :
+
+        statement(s);
+
+        break;
+
+        default :
+
+        statement(s);
+
+}
+
+
+        }
     }
 }
