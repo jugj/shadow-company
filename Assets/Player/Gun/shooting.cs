a164fabf58f042881fe4edd3d90cbc27f30d503b
Timo S <jugj@users.noreply.github.com>
2024-08-18 12:50:57 +0200

    Änderungen von Timo S um 12:50:56

--- Assets/Player/Gun/shooting.cs
+++ Assets/Player/Gun/shooting.cs
@@ -7,19 +7,25 @@ public class shooting : MonoBehaviour
 {
     public Transform shootingPoint;
     public GameObject bullet;
+    public Animator GunAnimator;
     private int weaponcooldown = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        if (weaponcooldown <= 10)
+        {
+        GunAnimator.SetBool("GunAnimation", false);
+        }
         if (Input.GetButtonDown("Fire1") && weaponcooldown <= 0)
         {
         Instantiate(bullet, shootingPoint.position, transform.rotation );
+        GunAnimator.SetBool("GunAnimation", true);
         weaponcooldown = 25;
         }
     }
